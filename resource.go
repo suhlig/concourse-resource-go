c535cb50db617bc794dd767f0b2fb057ce44e9f4
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2024-01-22 13:51:37 +0100

    Change resource interface to return response

--- resource.go
+++ resource.go
@@ -12,29 +12,29 @@ import (
 type Resource[S any, V any, P any] interface {
 	// Check is invoked to detect new versions of the resource.
 	//
-	// It is given the configured source and current version, and must append new versions to the response slice, in
+	// It is given the configured source and current version, and must return new versions as response, in
 	// chronological order (oldest first, including the requested version if it's still valid).
 	//
 	// [Check]: https://concourse-ci.org/implementing-resource-types.html#resource-check
-	Check(ctx context.Context, request CheckRequest[S, V], response *CheckResponse[V], log io.Writer) error
+	Check(ctx context.Context, request CheckRequest[S, V], log io.Writer) (CheckResponse[V], error)
 
 	// Get is invoked to fetch the resource and place it in the given directory.
 	//
 	// If is given the configured source, exact version of the resource to fetch, and configured parameters. It is
 	// also passed the destination directory where the state of the resource is to be placed.
 	//
-	// The function must add the fetched version to the response, and may add metadata.
+	// The function must return a response that describes the fetched version, and may add metadata.
 	//
 	// [In]: https://concourse-ci.org/implementing-resource-types.html#resource-in
-	Get(ctx context.Context, request GetRequest[S, V, P], response *Response[V], log io.Writer, destination string) error
+	Get(ctx context.Context, request GetRequest[S, V, P], log io.Writer, destination string) (*Response[V], error)
 
 	// Put is invoked to store the resource as it is given in the passed directory.
 	//
-	// If is given the configured source and configured parameters. The function must add the resulting version
-	// to the response, and may add metadata.
+	// If is given the configured source and configured parameters. The function must a response that describes the
+	// resulting version, and may add metadata.
 	//
 	// [Out]: https://concourse-ci.org/implementing-resource-types.html#resource-out
-	Put(ctx context.Context, request PutRequest[S, P], response *Response[V], log io.Writer, source string) error
+	Put(ctx context.Context, request PutRequest[S, P], log io.Writer, source string) (*Response[V], error)
 }
 
 type CheckRequest[S any, V any] struct {
--- test_models_test.go
+++ test_models_test.go
@@ -19,29 +19,28 @@ type Params struct{}
 
 type NullResource[S Source, V Version, P Params] struct{}
 
-func (r NullResource[S, V, P]) Check(ctx context.Context, request concourse.CheckRequest[Source, Version], response *concourse.CheckResponse[Version], log io.Writer) error {
-	return nil
+func (r NullResource[S, V, P]) Check(ctx context.Context, request concourse.CheckRequest[Source, Version], log io.Writer) (concourse.CheckResponse[Version], error) {
+	return concourse.CheckResponse[Version]{}, nil
 }
 
-func (r NullResource[S, V, P]) Get(ctx context.Context, request concourse.GetRequest[Source, Version, Params], response *concourse.Response[Version], log io.Writer, destination string) error {
-	return nil
+func (r NullResource[S, V, P]) Get(ctx context.Context, request concourse.GetRequest[Source, Version, Params], log io.Writer, destination string) (*concourse.Response[Version], error) {
+	return &concourse.Response[Version]{}, nil
 }
 
-func (r NullResource[S, V, P]) Put(ctx context.Context, request concourse.PutRequest[Source, Params], response *concourse.Response[Version], log io.Writer, source string) error {
-	return nil
+func (r NullResource[S, V, P]) Put(ctx context.Context, request concourse.PutRequest[Source, Params], log io.Writer, source string) (*concourse.Response[Version], error) {
+	return &concourse.Response[Version]{}, nil
 }
 
 type Troublemaker[S Source, V Version, P Params] struct{}
 
-func (r Troublemaker[S, V, P]) Check(ctx context.Context, request concourse.CheckRequest[Source, Version], response *concourse.CheckResponse[Version], log io.Writer) error {
-	*response = append(*response, Version{Number: 77})
-	return nil
+func (r Troublemaker[S, V, P]) Check(ctx context.Context, request concourse.CheckRequest[Source, Version], log io.Writer) (concourse.CheckResponse[Version], error) {
+	return concourse.CheckResponse[Version]{Version{Number: 77}}, nil
 }
 
-func (r Troublemaker[S, V, P]) Get(ctx context.Context, request concourse.GetRequest[Source, Version, Params], response *concourse.Response[Version], log io.Writer, destination string) error {
-	return nil
+func (r Troublemaker[S, V, P]) Get(ctx context.Context, request concourse.GetRequest[Source, Version, Params], log io.Writer, destination string) (*concourse.Response[Version], error) {
+	return &concourse.Response[Version]{}, nil
 }
 
-func (r Troublemaker[S, V, P]) Put(ctx context.Context, request concourse.PutRequest[Source, Params], response *concourse.Response[Version], log io.Writer, source string) error {
-	return nil
+func (r Troublemaker[S, V, P]) Put(ctx context.Context, request concourse.PutRequest[Source, Params], log io.Writer, source string) (*concourse.Response[Version], error) {
+	return &concourse.Response[Version]{}, nil
 }
